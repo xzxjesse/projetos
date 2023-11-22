da2e4fdac5ff46db9debcbb5240bfb71b3212379
xzxjesse <jessicaeveline121@gmail.com>
2023-11-22 20:42:44 -0300

    atualização

--- Python/sistema_notas/main.py
+++ Python/sistema_notas/main.py
@@ -16,24 +16,24 @@ def main():
         print("4. Realizar Provas")
         print("5. Sair")
 
-        escolha = input("Escolha uma opção (1, 2, 3, 4 ou 5): ")
+        escolha = input("Escolha uma opção: ")
 
-        if escolha == "1":
+        if escolha == "1" or "1. Cadastrar Usuário" or "Cadastrar Usuário":
             cadastrar_usuario(usuarios)
 
-        elif escolha == "2":
+        elif escolha == "2" or "2. Login" or "Login":
             usuario_autenticado = autenticar_usuario(usuarios)
             if usuario_autenticado:
                 print("Usuário autenticado.")
 
-        elif escolha == "3":
+        elif escolha == "3" or "3. Escolher Pesos e Quantidade de Provas" or "Escolher Pesos e Quantidade de Provas":
             if usuario_autenticado:
                 pesos = escolher_pesos()
                 quantidade_provas = escolher_quantidade_provas()
             else:
                 print("Você precisa fazer login antes de escolher pesos e quantidade de provas.")
 
-        elif escolha == "4":
+        elif escolha == "4" or "4. Realizar Provas" or "Realizar Provas":
             if usuario_autenticado and "pesos" in locals() and "quantidade_provas" in locals():
                 for i in range(1, quantidade_provas + 1):
                     quantidade_questoes = int(input(f"Digite a quantidade de questões para a Prova {i}: "))
@@ -42,7 +42,7 @@ def main():
             else:
                 print("Você precisa fazer login e escolher pesos/quantidade de provas antes de realizar as provas.")
 
-        elif escolha == "5":
+        elif escolha == "5" or "5. Sair" or "Sair":
             print("Programa encerrado.")
             break
 
