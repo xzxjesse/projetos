5fe0030662c4b42deba3e1c24606bfed517b0cdb
xzxjesse <jessicaeveline121@gmail.com>
2024-09-02 18:15:57 -0300

    API Vendedores

---
+++ C#/API/API_ProjetoFinal/API_ProjetoFinal/Models/VendedorModel.cs
@@ -0,0 +1,16 @@
+﻿namespace API_ProjetoFinal.Models
+{
+    public class VendedorModel
+    {
+        public int IdVendedor { get; set; }
+        public string Vendedor { get; set; } 
+        public decimal Comissao { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string CEP { get; set; }
+        public string UF { get; set; }
+        public string CPF { get; set; }
+    }
+
+}
